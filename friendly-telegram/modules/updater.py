5a3bb5166ead00db3eafc2339e237699055f672c
monolit <53448546+monolit@users.noreply.github.com>
2023-04-06 09:06:19 +0300

    edit updater source

--- friendly-telegram/modules/updater.py
+++ friendly-telegram/modules/updater.py
@@ -60,7 +60,7 @@ class UpdaterMod(loader.Module):
 
     def __init__(self):
         self.config = loader.ModuleConfig("GIT_ORIGIN_URL",
-                                          "https://github.com/GeekTG/Friendly-Telegram",
+                                          "https://github.com/monolit/ftg",
                                           lambda m: self.strings("origin_cfg_doc", m),
                                           "AUDIO", False, lambda m: self.strings("audio_cfg_doc", m))
 
